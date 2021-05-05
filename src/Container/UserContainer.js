c29cd1235e44bc2412316f1e56fc00f567ae93bf
Salman Sarwar <salmansarwar839@.com>
2021-05-05 12:53:25 +0500

    get method

--- src/Container/UserContainer.js
+++ src/Container/UserContainer.js
@@ -1,8 +1,7 @@
 import React, {useEffect} from 'react'
 import {connect} from 'react-redux'
-import ReduxDisplayer from '../Crud Displayer/ReduxDisplayer'
 import {fetchUsers} from '../thunk/Get Method/ThunkActions/actionCreator'
-
+import '../redux.css'
 
 function GetContainer ({userData, fetchUsers}) {
     useEffect(() => {
@@ -10,24 +9,74 @@ function GetContainer ({userData, fetchUsers}) {
     },[fetchUsers])
 
     return userData.loading ? (
-        <h2>Loading</h2>
+        <h2 className="container">Loading</h2>
     ) : userData.error ? (
         <h2>{userData.error}</h2>
     ) : (
         <div>
-            <h2>User List</h2>
-            <div>
+            <h2>Get Example</h2>
+            <div className="div">
                 {
                     userData && userData.users && userData.users.map(user => 
-                    <ReduxDisplayer
-                    id={user._id}
-                    nickname={user.nickname}
-                    name={user.username}
-                    pic={user.photo}
-                    email={user.email}
-                    password={user.password}
-                    value={user.__v}
-                    />)
+                    <table className='table'>
+                        <thead className='thead'>
+                        <tr>
+                        <th>PropertiesName</th>
+                        <th>Description</th>
+                        <th className="type">Type</th>
+                    </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                        <td >ID</td>
+                        <td>{user._id}</td>
+                        <td className="type">VarChar</td>
+                    </tr>
+
+
+                    <tr>
+                        <td>Nick Name</td>
+                        <td>{user.nickname}</td>
+                        <td className="type">String</td>
+                    </tr>
+
+
+                    <tr>
+                        <td>User Name</td>
+                        <td>{user.username}</td>
+                        <td className="type">String</td>
+                    </tr>
+
+
+                    <tr>
+                        <td>Picture</td>
+                        <td>{user.photo}</td>
+                        <td className="type">Blub</td>
+                    </tr>
+
+
+                    <tr>
+                        <td>Email</td>
+                        <td>{user.email}</td>
+                        <td className="type">String</td>
+                    </tr>
+
+
+                    <tr>
+                        <td>Password</td>
+                        <td>{user.password}</td>
+                        <td className="type">VarChar</td>
+                    </tr>
+
+
+                    <tr>
+                        <td>Value</td>
+                        <td>{user.__v}</td>
+                        <td className="type">Null</td>
+                    </tr>
+                        </tbody>
+                    </table>
+                        )
                 }
             </div>
         </div>
